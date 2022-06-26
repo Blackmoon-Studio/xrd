f3839c50525cf21f6e67c3ce249fb3c2817ed7ca
Autumn Leaf <107474172+info-ch4n@users.noreply.github.com>
2022-06-26 08:25:29 -0500

    Add files via upload

--- buildOnMusl.sh
+++ buildOnMusl.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 export RUSTFLAGS="-Ctarget-feature=-crt-static"
-exec cargo b
+cargo clean
+mkdir -p target/debug/plugins
+cd externs/plugins/xrdCore
+cargo b
+cp target/debug/libxrdCore.so ../../../target/debug/plugins
+cd ../../../
+cargo b
+exec rm target/debug/libxrdCore.so
